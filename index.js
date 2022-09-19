3a8197429dcc9782c206bf09601f35e5488b2683
Daniel Hiep Truong <danieltruong89@yahoo.com>
2022-09-18 20:54:23 -0600

    commit tree 25110e4477c6a4a4113e07c667adcf5465976d28

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValues){
+  if (someValues <= 400) {
+    return "This one is on me!"
+  } else if (someValues > 400, someValues <= 2000) {
+    return "That will be twenty bucks."
+  } else if (someValues > 2000, someValues <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (someValues > 2500) {
+    return 'No can do.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+return city === 'NYC'?'Ok, sounds good.' :'No go.'
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous': 
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
   // Write your code here!
 }
